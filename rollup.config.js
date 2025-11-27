859fd018aaa91f041eeceb0df13fadcd4c30fadb
jonathascastilho <jonathas.castilho@murabei.com>
2025-11-27 17:12:55 -0300

    first

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
+import image from "@rollup/plugin-image";
+
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      plugins: [terser()],
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    resolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    commonjs(),
+    image(),
+    babel({
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-react"],
+    }),
+    postcss(),
+  ],
+  external: ["react", "react-dom"],
+};
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// export {default as Toast} from "src/components/toast";
+// export * as Text from "src/components/text"; //for named export
